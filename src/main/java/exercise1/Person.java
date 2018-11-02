2f2b180c87fa20fb3d8e9e457d12f5f8527f43df
Umons-170961 <44571356+Umons-170961@users.noreply.github.com>
2018-11-02 12:38:18 +0100

    Fin exercice 1

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -3,4 +3,13 @@ package exercise1;
 class Person {
 	private int age;
 	private String name;
+	public Person(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	public String getName(){return name; }
+	public int getAge(){return age;}
+
+	@Override
+	public String toString(){return "Person{" + "nom=" + name + ", Age=" + age + "}";}
 }
